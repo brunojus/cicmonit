c8e823ac192e5ba69f37cfcbcab17818c4653fb6
brunojus <bruno_justino@icloud.com>
2016-04-09 00:25:11 -0300

    Revert "Cadastro de Professores"
    This reverts commit 4f776d24353914c747a5eb5ae6b29e1448487cef.

--- projetoRails/cicmonit/config/initializers/filter_parameter_logging.rb
+++ projetoRails/cicmonit/config/initializers/filter_parameter_logging.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-#Aqui não deveria ser encrypted_password??
 Rails.application.config.filter_parameters += [:password]
--- projetoRails/cicmonit/config/routes.rb
+++ projetoRails/cicmonit/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :professors
-  resources :professors
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
