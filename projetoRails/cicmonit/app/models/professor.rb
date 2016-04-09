da41e8dff5013f52bd1eac953af97f66dc02473b
brunojus <bruno_justino@icloud.com>
2016-04-09 00:30:26 -0300

    Revert "Revert "Revert "Cadastro de Professores"""
    This reverts commit 491635fa7478f986c36ab31eb80b0d2364cd2cdd.
    Signed-off-by: Bruno Justino <bruno_justino@icloud.com>
    Signed-off-by: Amanda Aline <amandaaline3@gmail.com>
    Signed-off-by: Farlen Rhenir <rhenir@globo.com>

--- projetoRails/cicmonit/app/models/professor.rb
+++
@@ -1,9 +0,0 @@
-class Professor < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-    validates :matricula, uniqueness: true
-
-
-end
