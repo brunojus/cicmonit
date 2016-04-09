491635fa7478f986c36ab31eb80b0d2364cd2cdd
brunojus <bruno_justino@icloud.com>
2016-04-09 00:25:43 -0300

    Revert "Revert "Cadastro de Professores""
    This reverts commit c8e823ac192e5ba69f37cfcbcab17818c4653fb6.
    
    Signed-off-by: brunojus <bruno_justino@icloud.com> amandaaline <amandaaline3@gmail.com> Farlen18 <rhenir@globo.com>

--- projetoRails/cicmonit/config/initializers/filter_parameter_logging.rb
+++ projetoRails/cicmonit/config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
+#Aqui não deveria ser encrypted_password??
 Rails.application.config.filter_parameters += [:password]
