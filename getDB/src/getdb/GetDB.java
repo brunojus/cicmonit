79bde920fbd490d8981125d12d1edfe37743d98d
Lincoln Abreu Barbosa <lincoln_abreu@aluno.unb.br>
2016-04-14 23:22:47 -0300

    Salvando em .txt
    Correção de erros e implementação do acesso à um arquivo efetuando
    backup de eventuais bancos existentes.

--- getDB/src/getdb/GetDB.java
+++ getDB/src/getdb/GetDB.java
@@ -5,7 +5,11 @@
  */
 package getdb;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,11 +23,31 @@ public class GetDB {
     public static void main(String[] args){
         //Endereço raiz no MW
         //String start = "oferta_campus.aspx";
+        //Endereço raiz do campus Darcy Ribeiro
+        //String start = "oferta_dep.aspx?cod=1";
         //endereço do CIC no MW
         String start = "oferta_dis.aspx?cod=116";
+        
+        //Inicializando os arquivos necessários
+        File file = new File("Banco/");
+        if(!file.exists())
+            file.mkdir();
+        file = new File("Banco/db.txt");
+        File fileold = new File("Banco/db-backup"+System.nanoTime()+".txt");
+        try {
+            if(!file.exists()) {
+                file.createNewFile();
+            }else{
+                file.renameTo(fileold);
+                file = new File("Banco/db.txt");
+                    file.createNewFile();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(navegador.class.getName()).log(Level.SEVERE, null, ex);
+        }
         try {
             //Início
-            getdb.navegador.spider(start);
+            getdb.navegador.acessodepartamento(start);
         } catch (MalformedURLException ex) {
             //Esse erro só ocorre em caso de a url acima estar incorreta
             return;
--- getDB/src/getdb/navegador.java
+++ getDB/src/getdb/navegador.java
@@ -6,78 +6,134 @@
 package getdb;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Linco
  */
 public class navegador {
-        //Navega através da arvore de links ,retornando Campus, Departamentos e/ou Matérias
-        static void spider(String link) throws MalformedURLException{
-            //condiçao para parar antes de acessar as turmas, pois nelas é outro formato de html
-            //o return sera substituido pela funçao de acesso as turmas
-            if (link.contains("oferta_dados.aspx?"))
-                //acessarturmas();
-                return;
-            //Url da raiz, todos os links sao somados à essa url
-            String startlink = "https://www.matriculaweb.unb.br/matriculaweb/graduacao/";
-            URL url = new URL(startlink.concat(link));
-            String html;
-            String [] parte1, parte2=null;
-            List<String> filtrado = new ArrayList<>();
-            //Todo o bloco necessário do html é lido como uma string
-            try{
-                html = readblock(url);
-            } catch (IOException ex) {
-                //Seria interessante o tratamento de um erro de comunicacao com o MW ou de falha no acesso a memória
-                return;
-            }
-            //Ocorre aqui a divisao e limpeza da string nas respectivas partes removendo a marcaçao html
-            parte1 = html.split("</tr>");
-            for(int i = 0; i<parte1.length; i++){
-                if(!(parte1[i].contains("<!-- TABELA MEIO -->") || parte1[i].contains("<!-- FIM TABELA MEIO -->"))){
-                    parte2 = parte1[i].split("<td>");
-                    for(int j = 0; j<parte2.length; j++){
-                        if(!(parte2[j].contains("<img") || parte2[j].contains("</b"))){
-                            if(!parte2[j].contains("<tr")){
-                                filtrado.add(parte2[j].replaceAll("<a href=", "").replaceAll("</td>", "").replaceAll("</a>", ""));
-                            }
+    //Navega através da arvore de links ,retornando Campus, Departamentos e/ou Matérias
+    static void acessodepartamento(String link) throws MalformedURLException{
+        String tobewritten = "", Num = "", NumSup="";
+        int flagwriten = 0;
+        //condiçao para parar antes de acessar as turmas, pois nelas é outro formato de html
+        //o return sera substituido pela funçao de acesso as turmas
+        if (link.contains("oferta_dados.aspx?"))
+            //acessarturmas();
+            return;
+        //Url da raiz, todos os links sao somados à essa url
+        String startlink = "https://www.matriculaweb.unb.br/matriculaweb/graduacao/";
+        URL url = new URL(startlink.concat(link));
+        String html;
+        String [] parte1, parte2=null;
+        List<String> filtrado = new ArrayList<>();
+        //Todo o bloco necessário do html é lido como uma string
+        try{
+            html = readblock(url);
+        } catch (IOException ex) {
+            //Seria interessante o tratamento de um erro de comunicacao com o MW ou de falha no acesso a memória
+            return;
+        }
+        //Ocorre aqui a divisao e limpeza da string nas respectivas partes removendo a marcaçao html
+        parte1 = html.split("</tr>");
+        for(int i = 0; i<parte1.length; i++){
+            if(!(parte1[i].contains("<!-- TABELA MEIO -->") || parte1[i].contains("<!-- FIM TABELA MEIO -->"))){
+                parte2 = parte1[i].split("<td>");
+                for(int j = 0; j<parte2.length; j++){
+                    if(!(parte2[j].contains("<img") || parte2[j].contains("</b"))){
+                        if(!parte2[j].contains("<tr")){
+                            filtrado.add(parte2[j].replaceAll("<a href=", "").replaceAll("</td>", "").replaceAll("</a>", ""));
                         }
                     }
                 }
             }
-            //Separaçao dos links dos nomes dos campus, matérias ou departamentos
-            // ocorre também a navegaçao por recursividade
+        }
+        //Separaçao dos links dos nomes dos campus, matérias ou departamentos
+        // ocorre também a navegaçao por recursividade
+        File file = new File("Banco/db.txt");
             for(int i = 0; i<filtrado.size(); i++){
+                //Estas linhas ditam a estrutura inicial do Banco
+                if(link.contains("oferta_dep")&& flagwriten == 0){
+                    Num = parte1[0].substring(parte1[0].indexOf("cod=")+4);
+                    tobewritten = "insert into departamento values (";
+                    flagwriten = 1;
+                }
+                if(link.contains("oferta_campus")&& flagwriten == 0){
+                    Num = parte1[0].substring(parte1[0].indexOf("cod=")+4);
+                    tobewritten = "insert into campus values (";
+                    flagwriten = 1;
+                }
+                if(link.contains("oferta_dis")){
+                     tobewritten = "insert into disciplinas values (";
+                    flagwriten = 1;
+                }
+                try (PrintWriter writer = new PrintWriter(new FileWriter(file, true))) {
                 if(filtrado.get(i).contains(">")){
-                   parte1 = filtrado.get(i).split(">");
-                   filtrado.set(i, parte1[1]);
-                   // aqui vai ser adicionado a saida pro banco
-                   System.out.println(filtrado.get(i));
-                   spider(parte1[0]);
-                }//else
+                    parte1 = filtrado.get(i).split(">");
+                    filtrado.set(i, parte1[1]);
+                    //Estas linhas são necessárias para navegação a partir de um departamento específico
+                    //devido à estrutura distinta da marcação
+                    if (link.contains("oferta_dis")){
+                        Num = parte1[0].substring(parte1[0].indexOf("cod=")+4,parte1[0].indexOf("&dep="));
+                        NumSup = parte1[0].substring(parte1[0].indexOf("dep=")+4);
+                        writer.println(tobewritten+Num+",'"+filtrado.get(i)+"',"+NumSup+");");
+                        flagwriten = 0;
+                    }
+                    //Estas linhas são necessárias para navegação a partir de um campus específico
+                    //devido à estrutura distinta da marcação
+                    else if(link.contains("oferta_dep")){
+                        Num = parte1[0].substring(parte1[0].indexOf("cod=")+4);
+                        NumSup = link.substring(parte1[0].indexOf("cod=")+4);
+                        writer.println(tobewritten+"'"+filtrado.get(i)+"',"+NumSup+");");
+                        flagwriten = 0;
+                    }else{
+                    //Estas linhas são necessárias para navegação a partir do início das ofertas do MW
+                    //devido à estrutura distinta da marcação
+                        Num = parte1[0].substring(parte1[0].indexOf("cod=")+4);
+                        writer.println(tobewritten+"'"+filtrado.get(i)+"');");
+                        flagwriten = 0;
+                    }
+                    writer.close();
+                    acessodepartamento(parte1[0]);
+                }else{
                     //esta linha é necessária para navegação a partir do campus, pois há a necessidade
                     //de uma ordem entre números identificadores, siglas e nomes
-                    //Aqui seria adicionada a saida pro banco caso o programa operasse em outros campus/departamentos
-                    //System.out.println(filtrado.get(i));
+                    try{
+                        Integer.parseInt(filtrado.get(i));
+                        tobewritten += filtrado.get(i) +",";
+                        flagwriten = 1;
+                    }catch(NumberFormatException ex){
+                        tobewritten += "'"+filtrado.get(i)+"',";
+                        flagwriten = 1;
+                    }
+                }
+            } catch (IOException ex) {
+                //Seria bom um tratamento de erro aqui
+                Logger.getLogger(navegador.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        //Funçao para acessar o html da turmas, retornando letra da turma horário e professor
-        static void acessarturmas (){
-            return;
-        }
-        
-        //Essa funçao retira só a parte necessária do html da pagina.
-        //A parte necessaria está contida entre os comentarios TABELA MEIO e FIM TABELA MEIO
-        static String readblock(URL url) throws IOException{
+    }
+
+    //Funçao para acessar o html da turmas, retornando letra da turma horário e professor
+    static void acessarturmas (){
+        return;
+    }
+
+    //Essa funçao retira só a parte necessária do html da pagina.
+    //A parte necessaria está contida entre os comentarios TABELA MEIO e FIM TABELA MEIO
+    static String readblock(URL url) throws IOException{
         int read = 0;
         InputStream source;
         BufferedReader buff;
@@ -97,4 +153,4 @@ public class navegador {
             source.close();
         return line;
     }
-    }
+}
