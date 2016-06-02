c7a962aac56e469e38ecfd045bdf803114cb6980
doiiido <140045023@aluno.unb.br>
2016-06-02 11:14:00 -0300

    Desfazendo a criaçao dos links

--- projetoRails/cicmonit/db/migrate/20160530053943_create_docentes.rb
+++
@@ -1,8 +0,0 @@
-class CreateDocentes < ActiveRecord::Migration
-  def change
-    create_table :docentes do |t|
-      t.string :nome
-      t.references :professor, index: true, foreign_key: true
-    end
-  end
-end
