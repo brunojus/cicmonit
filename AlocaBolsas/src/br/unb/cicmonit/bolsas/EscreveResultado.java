a6fd6a2e863357f90b6d822143d06fcc977329ff
doiiido <140045023@aluno.unb.br>
2016-07-13 10:39:09 -0300

    corrigindo erros e recompilando

--- AlocaBolsas/src/br/unb/cicmonit/bolsas/EscreveResultado.java
+++ AlocaBolsas/src/br/unb/cicmonit/bolsas/EscreveResultado.java
@@ -18,17 +18,17 @@ public class EscreveResultado {
 	public void Escrever (){
 		String tobewritten = "";
 		HashMap<Integer, Candidato> candidatos;
-		File file = new File("AlocaBolsas/result.sql");
+		File file = new File("db/AlocaBolsas/result.sql");
 		if(!file.exists())
             file.mkdir();
-        file = new File("AlocaBolsas/result.sql");
-        File fileold = new File("AlocaBolsas/result-backup"+System.nanoTime()+".sql");
+        file = new File("db/AlocaBolsas/result.sql");
+        File fileold = new File("db/AlocaBolsas/result-backup"+System.nanoTime()+".sql");
         try {
             if(!file.exists()) {
                 file.createNewFile();
             }else{
                 file.renameTo(fileold);
-                file = new File("AlocaBolsas/result.sql");
+                file = new File("db/AlocaBolsas/result.sql");
                     file.createNewFile();
             }
         } catch (IOException ex) {
